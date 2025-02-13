2bcca2cb1be7ccad15a9ce14eb7bcb678deb1406
Pasidu1 <163862208+Pasidu1@users.noreply.github.com>
2025-02-13 05:44:18 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,14 +5,14 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "KAVI-EXE=hZATlZpZ#Cio8v6U8v-QuG_6re7imL8HoTGFbURVKNgo_d1UQs_M",
+SESSION_ID: process.env.SESSION_ID || "KAVI-EXE=xJITVD4D#PD9eUpyhJmjYGq87u24lEVQkNGZixsR5gPSAcwDwnPk",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
-AUTO_REACT_STATUS: process.env.AUTO_REACT_STATUS || "false",
+AUTO_REACT_STATUS: process.env.AUTO_REACT_STATUS || "true",
 MODE: process.env.MODE || "private",
 ALWAYS_ONLINE : process.env.ALWAYS_ONLINE || "false",
 AUTO_TYPING: process.env.AUTO_TYPING || "false",
 AUTO_RECORDING: process.env.AUTO_RECORDING || "true",
-AUTO_STATUS_REPLY: process.env.AUTO_STATUS_REPLY || "false",
-AUTO_STATUS__MSG: process.env.AUTO_STATUS__MSG || "`SEEN YOUR STATUS BY 𝐊𝐀𝐕𝐈-𝐄𝐗𝐄🗿🍷`",
+AUTO_STATUS_REPLY: process.env.AUTO_STATUS_REPLY || "true",
+AUTO_STATUS__MSG: process.env.AUTO_STATUS__MSG || "`SEEN YOUR STATUS BY FRONEXT-MD🗿🍷`",
 ANTI_CALL: process.env.ANTI_CALL || "false",
 };
